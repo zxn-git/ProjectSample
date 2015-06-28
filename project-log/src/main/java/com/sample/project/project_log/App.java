6f18f5a1312606b20d01f0770e24079f5f6b4a69
zxn-test <461454959@qq.comm>
2015-06-28 16:05:58 +0800

    Signed-off-by: zxn-test <461454959@qq.comm>

--- project-log/src/main/java/com/sample/project/project_log/App.java
+++ project-log/src/main/java/com/sample/project/project_log/App.java
@@ -1,13 +1,26 @@
 package com.sample.project.project_log;
 
+import org.apache.log4j.Logger;
+
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	/**
+	 * 测试log4j 
+	 * @param args
+	 */
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Logger logger = Logger.getLogger("测试");
+        logger.error("error");
+        logger.debug("debug");;
+        logger.info("info");
+        
+        
     }
 }
