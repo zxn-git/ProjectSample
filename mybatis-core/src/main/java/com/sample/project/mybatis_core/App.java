68f0282815616974fb7fad6a2aa8c2519770a347
zxn-git <461454959@qq.com>
2015-06-28 08:48:32 +0800

    项目整体架构，持续完善中

---
+++ mybatis-core/src/main/java/com/sample/project/mybatis_core/App.java
@@ -0,0 +1,13 @@
+package com.sample.project.mybatis_core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ spring-service/src/main/java/com/sample/project/spring_service/App.java
@@ -0,0 +1,13 @@
+package com.sample.project.spring_service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
