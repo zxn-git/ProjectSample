86b5e7ff9da0d2db12d0dda61252592cdef136af
zxn-test <461454959@qq.comm>
2015-06-30 16:08:04 +0800

    配置文件、一些建表语句

---
+++ mybatis-core/sql/createTable.sql
@@ -0,0 +1,9 @@
+--创建库
+create database test;
+use test;
+--创建表
+CREATE TABLE users(id INT PRIMARY KEY AUTO_INCREMENT, NAME VARCHAR(20), age INT);
+INSERT INTO users(NAME, age) VALUES('Tom', 12);
+INSERT INTO users(NAME, age) VALUES('Jack', 11);
+
+
