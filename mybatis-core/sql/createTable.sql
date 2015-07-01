20337b6668e5162a6c95191a1c3e2dda207870df
zxn-git <461454959@qq.com>
2015-07-01 23:44:32 +0800

    完善简单的测试

--- mybatis-core/sql/createTable.sql
+++ mybatis-core/sql/createTable.sql
@@ -2,8 +2,8 @@
 create database test;
 use test;
 --创建表
-CREATE TABLE users(id INT PRIMARY KEY AUTO_INCREMENT, NAME VARCHAR(20), age INT);
-INSERT INTO users(NAME, age) VALUES('Tom', 12);
-INSERT INTO users(NAME, age) VALUES('Jack', 11);
+CREATE TABLE user(id INT PRIMARY KEY AUTO_INCREMENT, NAME VARCHAR(20), age INT);
+INSERT INTO user(NAME, age) VALUES('Tom', 12);
+INSERT INTO user(NAME, age) VALUES('Jack', 11);
 
 
--- mybatis-core/src/test/java/com/sample/project/mybatis_core/NoInterfaceTest.java
+++ mybatis-core/src/test/java/com/sample/project/mybatis_core/NoInterfaceTest.java
@@ -1,20 +1,14 @@
 package com.sample.project.mybatis_core;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.Reader;
-import java.sql.Connection;
 
 import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.Configuration;
-import org.apache.ibatis.session.ExecutorType;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.apache.ibatis.session.TransactionIsolationLevel;
+
+import com.sample.project.mybatis_core.model.User;
 
 /**
  * 没有写dao（接口）情况 根据mapping 和model 操作数据
@@ -24,18 +18,23 @@ import org.apache.ibatis.session.TransactionIsolationLevel;
  */
 public class NoInterfaceTest {
 
-	// 配置文件字节流
-	private static InputStream resourceAsStream;
-
+	private static Reader reader;
 	private static SqlSessionFactory sqlSessionFactory;
 
 	static {
-		resourceAsStream = NoInterfaceTest.class.getResourceAsStream("");
-		sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+		try {
+			reader = Resources.getResourceAsReader("mybatis.xml");
+			sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static void main(String[] args) {
-
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		User user = (User) sqlSession.selectOne("com.sample.project.mybatis_core.mapping.selectUserByID", 1);
+		System.err.println(user.getName());
+		System.err.println(user.getAge());
 	}
 
 }
